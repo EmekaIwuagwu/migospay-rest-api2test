53cf89133041b2f873a7c6f0dbc467faa2c34f15
Emeka Iwuagwu <emeka1987ng@gmail.com>
2022-10-29 18:08:54 +0100

    my changes

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from app import app
 from flask_mysqldb import MySQL
 
 
-app.config['MYSQL_USER'] = 'bbc7c2522deece'
-app.config['MYSQL_PASSWORD'] = '8fdba964'
-app.config['MYSQL_DB'] = 'migospay'
+app.config['MYSQL_USER'] = 'bce595e396ecba'
+app.config['MYSQL_PASSWORD'] = '14d18fbb'
+app.config['MYSQL_DB'] = 'heroku_7880d8ac0ebd47f'
 app.config['MYSQL_HOST'] = 'eu-cdbr-west-03.cleardb.net'
 
 mysql = MySQL(app)
--- main.py
+++ main.py
@@ -258,4 +258,4 @@ def showMessage(error=None):
     return response
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
