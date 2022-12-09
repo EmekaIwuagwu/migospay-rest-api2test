ebefb8d1ce47bdab8145efa1891c6e0ad50dd851
Emeka Iwuagwu <emeka1987ng@gmail.com>
2022-12-09 08:10:52 +0100

    my changes

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from app import app
 from flask_mysqldb import MySQL
 
 
-app.config['MYSQL_USER'] = 'bce595e396ecba'
-app.config['MYSQL_PASSWORD'] = '14d18fbb'
-app.config['MYSQL_DB'] = 'heroku_7880d8ac0ebd47f'
-app.config['MYSQL_HOST'] = 'eu-cdbr-west-03.cleardb.net'
+app.config['MYSQL_USER'] = 'b6cf5c996b7e5e'
+app.config['MYSQL_PASSWORD'] = '46d6dbe5'
+app.config['MYSQL_DB'] = 'heroku_61e683edf0d1de4'
+app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
 
 mysql = MySQL(app)
 
